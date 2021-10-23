d141bf0ee513974475b98f25201a6e04dcdaaa2d
Bill Koutsis <79660306+BillKoutsis@users.noreply.github.com>
2021-10-24 00:03:02 +0300

    Add files via upload

---
+++ php/kiria_selidha.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<link rel="stylesheet" href="../css/kiria_selidha.css"  type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Language" content="el">
+<title> Αρχική σελίδα </title>
+<link rel="shortcut icon" type="image/x-icon" href="../media/favicon.ico">
+<meta name="google-translate-customization" content="b1af73f9c997c045-c4ba2f774844d82e-gdf0fdc3f0fd4f93d-2a"></meta>
+</head>
+
+<body>
+<div id="im">
+Date
+<script type="text/javascript">
+var d = new Date()
+document.write(d.getDate())
+document.write(".")
+document.write(d.getMonth() + 1)
+document.write(".")
+document.write(d.getFullYear())
+
+</script>
+</div>
+
+<br>
+
+<div id="ora">
+Time
+<script type="text/javascript">
+
+var d = new Date()
+
+document.write(d.getHours())
+
+document.write(".")
+
+document.write(d.getMinutes())
+
+document.write(".")
+
+document.write(d.getSeconds())
+
+</script>
+</div>
+
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'el', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<div id="menu">
+<ul>
+<li> <a href="../php/kiria_selidha.php"> Αρχική </a></li>
+<li> <a href="../html/podhosfairo.html"> Ποδόσφαιρο </a></li>
+<li> <a href="../html/basket.html"> Μπάσκετ </a> </li>
+<li> <a href="../html/volley.html"> Βόλεϊ </a> </li>
+<li> <a href="../html/tennis.html"> Τένις </a> </li>
+</ul>
+</div>
+
+<div id="main_article">
+<h1> Καλωσήρθατε στην ιστοσελίδα μας </h1>
+<p> Σε αυτή την ιστοσελίδα θα βρείτε μερικές πληροφορίες για τα πιο γνωστά αθλήματα τη σημερινή εποχή καθώς και μερικά βίντεο με υπέροχες φάσεις από το κάθε άθλημα ώστε να μπορέσετε να επιλέξετε ευκολότερα το άθλημα που σας ταιριάζει και σας ελκύει περισσότερο </p>
+<div class="image5">
+<img src="../media/e9ztyrvozfbal3d9qstu.jpg" width="600" height="500">
+</div>
+</div>
+</body>
+
+
+</html>
